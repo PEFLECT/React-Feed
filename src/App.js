517c2fa2a732f727b34d9ec527e29fd51e1f58ec
PEFLECT <path.tanapat@gmail.com>
2019-09-27 11:44:59 +0700

    title and input

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    
+    
+    
+    return (
+      <div className="App">
+        {/*  JSX  */}
+        <table className="Navbar">
+          <tbody>
+            <tr>
+              <td>
+                <img src={require('./assets/logo.svg')} width="50"></img>
+              </td>
+              <tb>
+                Tanapat "PEFLECT" Dejpunyawat
+              </tb>
+            </tr>
+          </tbody>
+        </table>
+        <input style={{fontSize: 24, display: 'block', width: '100%', paddingLeft: 8}} placeholder="Enter Your Movie" onChange={(event) => {console.log(event.target.value)}}></input>
+      </div>
+    );
+  
 }
 
 export default App;
